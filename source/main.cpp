b814ff907b0426da0df68dcff2864e045093b573
Asger Nyman Christiansen <asny@dtu.dk>
2015-10-03 10:17:56 +0300

    Added project

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 03/10/15.
+//  Copyright (c) 2015 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
