3549131deb9c34d353510f0af2d1f021316ea3aa
Asger Nyman Christiansen <asny@dtu.dk>
2015-10-03 23:08:10 +0200

    Created model.

---
+++ source/model.cpp
@@ -0,0 +1,9 @@
+//
+//  model.cpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 03/10/15.
+//  Copyright © 2015 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include "model.hpp"
---
+++ source/model.hpp
@@ -0,0 +1,16 @@
+//
+//  model.hpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 03/10/15.
+//  Copyright © 2015 Asger Nyman Christiansen. All rights reserved.
+//
+
+#pragma once
+
+class Model {
+    
+    
+public:
+    
+};
