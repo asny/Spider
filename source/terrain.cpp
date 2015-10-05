0b211bf8f50b5919d973170c2eb2cb81598823e8
Asger Nyman Christiansen <asny@dtu.dk>
2015-10-05 08:45:33 +0200

    Added Terrain class.

---
+++ source/terrain.cpp
@@ -0,0 +1,9 @@
+//
+//  terrain.cpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 05/10/15.
+//  Copyright © 2015 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include "terrain.hpp"
---
+++ source/terrain.hpp
@@ -0,0 +1,27 @@
+//
+//  terrain.hpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 05/10/15.
+//  Copyright © 2015 Asger Nyman Christiansen. All rights reserved.
+//
+
+#pragma once
+
+#include <vector>
+
+class TerrainPatch {
+    std::vector<std::vector<double>> heightmap;
+    
+public:
+    
+};
+
+class Terrain {
+    
+    
+    
+    
+public:
+    
+};
