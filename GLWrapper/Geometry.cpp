567d436eea3351777b4715c0acf0a98bfd4d6590
Asger Nyman Christiansen <asny@dtu.dk>
2016-07-27 21:29:09 +0200

    Added Geometry class

---
+++ GLWrapper/Geometry.cpp
@@ -0,0 +1,9 @@
+//
+//  Geometry.cpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 26/07/16.
+//  Copyright © 2016 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include "Geometry.hpp"
---
+++ GLWrapper/Geometry.hpp
@@ -0,0 +1,14 @@
+//
+//  Geometry.hpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 26/07/16.
+//  Copyright © 2016 Asger Nyman Christiansen. All rights reserved.
+//
+
+#pragma once
+
+class Geometry
+{
+    
+};
