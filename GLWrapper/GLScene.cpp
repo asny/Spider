9acb0852853b6a026419eee7fea65c1ef40921af
Asger Nyman Christiansen <asny@dtu.dk>
2016-07-30 21:18:34 +0200

    Added GLScene

---
+++ GLWrapper/GLScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GLScene.cpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 30/07/16.
+//  Copyright © 2016 Asger Nyman Christiansen. All rights reserved.
+//
+
+#include "GLScene.hpp"
---
+++ GLWrapper/GLScene.hpp
@@ -0,0 +1,11 @@
+//
+//  GLScene.hpp
+//  Spider
+//
+//  Created by Asger Nyman Christiansen on 30/07/16.
+//  Copyright © 2016 Asger Nyman Christiansen. All rights reserved.
+//
+
+#pragma once
+
+
