8cdd8309d9400c61a70d5a26dc427c3c48615243
Asger Nyman Christiansen <asny@dtu.dk>
2016-08-23 09:37:53 +0200

    Bug fix for updating the vertex attributes each render frame.

--- GLWrapper/GLObject.hpp
+++ GLWrapper/GLObject.hpp
@@ -23,42 +23,42 @@ namespace oogl
         std::shared_ptr<GLMaterial> material;
         
         GLuint array_id;
-        std::vector<GLVertexAttribute<glm::vec2>> vec2_vertex_attributes;
-        std::vector<GLVertexAttribute<glm::vec3>> vec3_vertex_attributes;
+        std::vector<std::shared_ptr<GLVertexAttribute<glm::vec2>>> vec2_vertex_attributes;
+        std::vector<std::shared_ptr<GLVertexAttribute<glm::vec3>>> vec3_vertex_attributes;
         
         // Transformation
         glm::mat4 modelMatrix = glm::mat4(1.);
         
         template<class T>
-        void update_attribute(GLVertexAttribute<T>& attribute)
+        void update_attribute(std::shared_ptr<GLVertexAttribute<T>> attribute)
         {
-            if(attribute.is_up_to_date())
+            if(attribute->is_up_to_date())
                 return;
             if(geometry->get_no_faces() > 0)
             {
                 for(auto face = geometry->faces_begin(); face != geometry->faces_end(); face = face->next())
                 {
-                    attribute.add_data_at(*face->v1());
-                    attribute.add_data_at(*face->v2());
-                    attribute.add_data_at(*face->v3());
+                    attribute->add_data_at(*face->v1());
+                    attribute->add_data_at(*face->v2());
+                    attribute->add_data_at(*face->v3());
                 }
             }
             else if(geometry->get_no_edges() > 0)
             {
                 for(auto edge = geometry->edges_begin(); edge != geometry->edges_end(); edge = edge->next())
                 {
-                    attribute.add_data_at(*edge->v1());
-                    attribute.add_data_at(*edge->v2());
+                    attribute->add_data_at(*edge->v1());
+                    attribute->add_data_at(*edge->v2());
                 }
             }
             else if(geometry->get_no_vertices() > 0)
             {
                 for(auto vertex = geometry->vertices_begin(); vertex != geometry->vertices_end(); vertex = vertex->next())
                 {
-                    attribute.add_data_at(*vertex);
+                    attribute->add_data_at(*vertex);
                 }
             }
-            attribute.send_data();
+            attribute->send_data();
         }
         
     public:
@@ -69,14 +69,14 @@ namespace oogl
         {
             glBindVertexArray(array_id);
             auto location = material->get_attribute_location(name);
-            vec2_vertex_attributes.push_back(GLVertexAttribute<glm::vec2>(location, attribute));
+            vec2_vertex_attributes.push_back(std::shared_ptr<GLVertexAttribute<glm::vec2>>(new GLVertexAttribute<glm::vec2>(location, attribute)));
         }
         
         void use_attribute(std::string name, std::shared_ptr<geogo::Attribute<geogo::VertexID, glm::vec3>> attribute)
         {
             glBindVertexArray(array_id);
             auto location = material->get_attribute_location(name);
-            vec3_vertex_attributes.push_back(GLVertexAttribute<glm::vec3>(location, attribute));
+            vec3_vertex_attributes.push_back(std::shared_ptr<GLVertexAttribute<glm::vec3>>(new GLVertexAttribute<glm::vec3>(location, attribute)));
         }
         
         /**
