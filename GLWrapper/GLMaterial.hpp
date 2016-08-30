dca4f30256ec2776471773935e9c9898899e0cc2
Asger Nyman Christiansen <asny@dtu.dk>
2016-08-30 20:48:38 +0200

    Using unique ptrs in GLMaterial.

--- GLWrapper/GLMaterial.hpp
+++ GLWrapper/GLMaterial.hpp
@@ -44,25 +44,18 @@ namespace oogl
     
     class GLStandardMaterial : public GLMaterial
     {
-        GLUniform<glm::vec4>* ambient;
-        GLUniform<glm::vec4>* diffuse;
-        GLUniform<glm::vec4>* specular;
+        std::unique_ptr<GLUniform<glm::vec4>> ambient;
+        std::unique_ptr<GLUniform<glm::vec4>> diffuse;
+        std::unique_ptr<GLUniform<glm::vec4>> specular;
     public:
         
         GLStandardMaterial(const glm::vec4& _ambient, const glm::vec4& _diffuse, const glm::vec4& _specular)
         {
             shader = std::make_shared<GLShader>("shaders/phong.vert",  "shaders/phong.frag");
             
-            ambient = new GLUniform<glm::vec4>(get_uniform_location("ambientMat"), _ambient);
-            diffuse = new GLUniform<glm::vec4>(get_uniform_location("diffuseMat"), _diffuse);
-            specular = new GLUniform<glm::vec4>(get_uniform_location("specMat"), _specular);
-        }
-        
-        ~GLStandardMaterial()
-        {
-            delete ambient;
-            delete diffuse;
-            delete specular;
+            ambient = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("ambientMat"), _ambient));
+            diffuse = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("diffuseMat"), _diffuse));
+            specular = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("specMat"), _specular));
         }
         
         void pre_draw();
@@ -71,19 +64,14 @@ namespace oogl
     class GLTextureMaterial : public GLMaterial
     {
         std::shared_ptr<GLTexture> texture;
-        GLUniform<int>* texture_id_uniform;
+        std::unique_ptr<GLUniform<int>> texture_id_uniform;
         std::shared_ptr<int> texture_id = std::make_shared<int>(0);
     public:
         
         GLTextureMaterial(std::shared_ptr<GLTexture> _texture) : texture(_texture)
         {
             shader = std::make_shared<GLShader>("shaders/texture.vert",  "shaders/texture.frag");
-            texture_id_uniform = new GLUniform<int>(get_uniform_location("texture0"), texture_id);
-        }
-        
-        ~GLTextureMaterial()
-        {
-            delete texture_id_uniform;
+            texture_id_uniform = std::unique_ptr<GLUniform<int>>(new GLUniform<int>(get_uniform_location("texture0"), texture_id));
         }
         
         void pre_draw();
@@ -92,19 +80,14 @@ namespace oogl
     class GLSkyboxMaterial : public GLMaterial
     {
         std::shared_ptr<GLTexture3D> texture;
-        GLUniform<int>* texture_id_uniform;
+        std::unique_ptr<GLUniform<int>> texture_id_uniform;
         std::shared_ptr<int> texture_id = std::make_shared<int>(0);
     public:
         
         GLSkyboxMaterial(std::shared_ptr<GLTexture3D> _texture) : texture(_texture)
         {
             shader = std::make_shared<GLShader>("shaders/skybox.vert",  "shaders/skybox.frag");
-            texture_id_uniform = new GLUniform<int>(get_uniform_location("texture0"), texture_id);
-        }
-        
-        ~GLSkyboxMaterial()
-        {
-            delete texture_id_uniform;
+            texture_id_uniform = std::unique_ptr<GLUniform<int>>(new GLUniform<int>(get_uniform_location("texture0"), texture_id));
         }
         
         void pre_draw();
@@ -112,22 +95,17 @@ namespace oogl
     
     class GLGrassMaterial : public GLMaterial
     {
-        GLUniform<glm::vec4>* ambient;
-        GLUniform<glm::vec4>* diffuse;
+        std::unique_ptr<GLUniform<glm::vec4>> ambient;
+        std::unique_ptr<GLUniform<glm::vec4>> diffuse;
     public:
         
         GLGrassMaterial(const glm::vec4& _ambient, const glm::vec4& _diffuse)
         {
             shader = std::make_shared<GLShader>("shaders/pre_geom.vert",  "shaders/grass.frag", "shaders/grass.geom");
             cull_back_faces = false;
-            ambient = new GLUniform<glm::vec4>(get_uniform_location("ambientMat"), _ambient);
-            diffuse = new GLUniform<glm::vec4>(get_uniform_location("diffuseMat"), _diffuse);
-        }
-        
-        ~GLGrassMaterial()
-        {
-            delete ambient;
-            delete diffuse;
+            
+            ambient = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("ambientMat"), _ambient));
+            diffuse = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("diffuseMat"), _diffuse));
         }
         
         void pre_draw();
@@ -135,25 +113,18 @@ namespace oogl
     
     class GLSpiderLegsMaterial : public GLMaterial
     {
-        GLUniform<glm::vec4>* ambient;
-        GLUniform<glm::vec4>* diffuse;
-        GLUniform<glm::vec4>* specular;
+        std::unique_ptr<GLUniform<glm::vec4>> ambient;
+        std::unique_ptr<GLUniform<glm::vec4>> diffuse;
+        std::unique_ptr<GLUniform<glm::vec4>> specular;
     public:
         
         GLSpiderLegsMaterial(const glm::vec4& _ambient, const glm::vec4& _diffuse, glm::vec4 _specular)
         {
             shader = std::make_shared<GLShader>("shaders/pre_geom.vert",  "shaders/phong.frag", "shaders/spider_legs.geom");
             
-            ambient = new GLUniform<glm::vec4>(get_uniform_location("ambientMat"), _ambient);
-            diffuse = new GLUniform<glm::vec4>(get_uniform_location("diffuseMat"), _diffuse);
-            specular = new GLUniform<glm::vec4>(get_uniform_location("specMat"), _specular);
-        }
-        
-        ~GLSpiderLegsMaterial()
-        {
-            delete ambient;
-            delete diffuse;
-            delete specular;
+            ambient = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("ambientMat"), _ambient));
+            diffuse = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("diffuseMat"), _diffuse));
+            specular = std::unique_ptr<GLUniform<glm::vec4>>(new GLUniform<glm::vec4>(get_uniform_location("specMat"), _specular));
         }
         
         void pre_draw()
