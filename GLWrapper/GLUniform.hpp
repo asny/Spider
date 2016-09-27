d033f6811f296cc4caec922084775adc7ac4b2a1
Asger Nyman Christiansen <asny@dtu.dk>
2016-09-27 08:32:00 +0200

    Namespace for GLUniform

--- GLWrapper/GLUniform.hpp
+++ GLWrapper/GLUniform.hpp
@@ -10,58 +10,61 @@
 
 #include "GLUtility.h"
 
-template <class ValueType>
-class GLUniform
+namespace oogl
 {
-public:
-    
-    GLUniform(GLuint _location, const ValueType& _value) : value(std::make_shared<ValueType>(_value)), location(_location)
+    template <class ValueType>
+    class GLUniform
     {
+    public:
         
-    }
-    
-    GLUniform(GLuint _location, const std::shared_ptr<ValueType> _value) : value(_value), location(_location)
-    {
+        GLUniform(GLuint _location, const ValueType& _value) : value(std::make_shared<ValueType>(_value)), location(_location)
+        {
+            
+        }
         
-    }
-    
-    void use()
-    {
-        set(*value);
-    }
-    
-private:
-    const std::shared_ptr<ValueType> value;
-    GLuint location;
-    
-    void set(float value)
-    {
-        glUniform1f(location, value);
-    }
-    
-    void set(int value)
-    {
-        glUniform1i(location, value);
-    }
-    
-    void set(const glm::vec2& value)
-    {
-        glUniform2fv(location, 1, &value[0]);
-    }
-    
-    void set(const glm::vec3& value)
-    {
-        glUniform3fv(location, 1, &value[0]);
-    }
-    
-    void set(const glm::vec4& value)
-    {
-        glUniform4fv(location, 1, &value[0]);
-    }
-    
-    void set(const glm::mat4& value)
-    {
-        glUniformMatrix4fv(location, 1, GL_FALSE, &value[0][0]);
-    }
-    
-};
+        GLUniform(GLuint _location, const std::shared_ptr<ValueType> _value) : value(_value), location(_location)
+        {
+            
+        }
+        
+        void use()
+        {
+            set(*value);
+        }
+        
+    private:
+        const std::shared_ptr<ValueType> value;
+        GLuint location;
+        
+        void set(float value)
+        {
+            glUniform1f(location, value);
+        }
+        
+        void set(int value)
+        {
+            glUniform1i(location, value);
+        }
+        
+        void set(const glm::vec2& value)
+        {
+            glUniform2fv(location, 1, &value[0]);
+        }
+        
+        void set(const glm::vec3& value)
+        {
+            glUniform3fv(location, 1, &value[0]);
+        }
+        
+        void set(const glm::vec4& value)
+        {
+            glUniform4fv(location, 1, &value[0]);
+        }
+        
+        void set(const glm::mat4& value)
+        {
+            glUniformMatrix4fv(location, 1, GL_FALSE, &value[0][0]);
+        }
+        
+    };
+}
