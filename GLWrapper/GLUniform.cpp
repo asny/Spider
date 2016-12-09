a4fab868064b3909c8d5e3a83335b7b5d332fa2a
Asger Nyman Christiansen <asgernyman@gmail.com>
2016-12-09 14:49:12 +0100

    Removed empty OOGL cpp files.

--- GLWrapper/GLUniform.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  GLUniform.cpp
-//  Spider
-//
-//  Created by Asger Nyman Christiansen on 23/08/16.
-//  Copyright © 2016 Asger Nyman Christiansen. All rights reserved.
-//
-
-#include "GLUniform.hpp"
