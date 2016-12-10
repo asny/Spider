a8521b6580b864975d6943aca3ff5277d57015e2
Asger Nyman Christiansen <asgernyman@gmail.com>
2016-12-10 19:16:41 +0100

    Removed GLObject.cpp

--- GLWrapper/GLObject.hpp
+++ GLWrapper/GLObject.hpp
@@ -64,12 +64,64 @@ namespace oogl
         
     public:
         
-        GLObject(std::shared_ptr<geogo::Mesh> geometry, std::shared_ptr<GLMaterial> material);
+        GLObject(std::shared_ptr<geogo::Mesh> geometry, std::shared_ptr<GLMaterial> material) : material(material), geometry(geometry)
+        {
+            // Generate and bind array
+            glGenVertexArrays(1, &array_id);
+            glBindVertexArray(array_id);
+            
+            vec3_vertex_attributes.push_back(material->create_attribute("position", geometry->position()));
+            material->create_attributes(vec2_vertex_attributes, vec3_vertex_attributes);
+        }
         
         /**
          Draws the object.
          */
-        void draw();
+        void draw()
+        {
+            // Infer draw mode
+            GLenum drawmode;
+            int no_vertices;
+            if(geometry->get_no_faces() > 0)
+            {
+                drawmode = GL_TRIANGLES;
+                no_vertices = geometry->get_no_faces() * 3;
+            }
+            else if(geometry->get_no_edges() > 0)
+            {
+                drawmode = GL_LINES;
+                no_vertices = geometry->get_no_edges() * 2;
+            }
+            else if(geometry->get_no_vertices() > 0)
+            {
+                drawmode = GL_POINTS;
+                no_vertices = geometry->get_no_vertices();
+            }
+            else
+            {
+                return;
+            }
+            
+            // Update buffers if necessary
+            for (auto glAttribute : vec2_vertex_attributes)
+            {
+                update_attribute(glAttribute);
+            }
+            
+            for (auto glAttribute : vec3_vertex_attributes)
+            {
+                update_attribute(glAttribute);
+            }
+            
+            // Use material specific uniforms and states
+            material->pre_draw();
+            
+            // Bind vertex array and draw
+            glBindVertexArray(array_id);
+            glDrawArrays(drawmode, 0, no_vertices);
+            
+            check_gl_error();
+        }
         
         /**
          Set the model matrix.
