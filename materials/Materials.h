a537e9cb919496f9173c399926ae2be791b837a1
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-01-19 09:05:56 +0100

    Made grass material deferred.

--- materials/Materials.h
+++ materials/Materials.h
@@ -12,24 +12,23 @@
 #include "materials/GLStandardMaterial.h"
 #include "materials/GLTextureMaterial.h"
 #include "materials/GLSkyboxMaterial.h"
+#include "materials/GLColorMaterial.h"
 
 class GLGrassMaterial : public gle::GLMaterial
 {
     std::shared_ptr<glm::vec3> spider_position;
     std::shared_ptr<glm::vec3> wind;
-    
-    glm::vec3 ambient, diffuse;
-    float opacity;
+    glm::vec3 color;
 public:
     
-    GLGrassMaterial(const std::shared_ptr<glm::vec3> _spider_position, const std::shared_ptr<glm::vec3> _wind, const glm::vec3& _ambient, const glm::vec3& _diffuse, float _opacity) : spider_position(_spider_position), wind(_wind), ambient(_ambient), diffuse(_diffuse), opacity(_opacity)
+    GLGrassMaterial(const std::shared_ptr<glm::vec3> _spider_position,const std::shared_ptr<glm::vec3> _wind, const glm::vec3& _color) : spider_position(_spider_position), wind(_wind), color(_color)
     {
-        shader = gle::GLShader::create_or_get("../GLEngine/shaders/pre_geom.vert",  "shaders/grass.frag", "shaders/grass.geom");
+        shader = gle::GLShader::create_or_get("../GLEngine/shaders/pre_geom.vert",  "../GLEngine/shaders/color_material.frag", "shaders/grass.geom");
     }
     
     bool should_draw(gle::DrawPassMode draw_pass)
     {
-        return draw_pass == gle::FORWARD;
+        return draw_pass == gle::DEFERRED;
     }
     
     void create_attributes(std::shared_ptr<mesh::Mesh> geometry, std::vector<std::shared_ptr<gle::GLVertexAttribute<glm::vec2>>>& vec2_vertex_attributes,
@@ -41,12 +40,11 @@ public:
     void pre_draw(const glm::vec3& camera_position, const glm::mat4& model, const glm::mat4& view, const glm::mat4& projection)
     {
         gle::GLState::depth_test(true);
-        gle::GLState::depth_write(opacity >= 0.999);
+        gle::GLState::depth_write(true);
         gle::GLState::cull_back_faces(false);
         
         auto modelView = view * model;
         
-        gle::GLUniform::use(shader, "VMatrix", view);
         gle::GLUniform::use(shader, "MVMatrix", modelView);
         gle::GLUniform::use(shader, "PMatrix", projection);
         gle::GLUniform::use(shader, "NMatrix", inverseTranspose(modelView));
@@ -54,9 +52,7 @@ public:
         gle::GLUniform::use(shader, "spiderPosition", *spider_position);
         gle::GLUniform::use(shader, "wind", *wind);
         
-        gle::GLUniform::use(shader, "ambientMat", ambient);
-        gle::GLUniform::use(shader, "diffuseMat", diffuse);
-        gle::GLUniform::use(shader, "opacity", opacity);
+        gle::GLUniform::use(shader, "materialColor", color);
     }
 };
 
