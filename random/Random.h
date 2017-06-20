7ffd145edfd363e4d2370ac136e89e268f9c9e66
Asger Nyman Christiansen <asgernyman@gmail.com>
2017-06-20 08:52:33 +0200

    Added hemisphere direction to Random.

--- random/Random.h
+++ random/Random.h
@@ -9,14 +9,16 @@
 #pragma once
 
 #include "simplexnoise.h"
+#include "gtx/rotate_vector.hpp"
 
 class Random {
     
     
 public:
-    static double value(double min, double max)
+    template<typename T>
+    static T value(T min, T max)
     {
-        return (max - min) * (double)rand()/(double)RAND_MAX + min;
+        return (max - min) * (T)rand()/(T)RAND_MAX + min;
     }
     
     static glm::vec3 direction()
@@ -25,6 +27,14 @@ public:
         double phi = value(0., M_PI);
         double sin_phi = sin(phi);
         return glm::vec3(cos(theta) * sin_phi, cos(phi), sin(theta) * sin_phi);
+    }
+    
+    static glm::vec3 hemisphere_direction(const glm::vec3& direction)
+    {
+        double theta = value(0., 2. * M_PI);
+        double phi = value(0., 0.5 * M_PI);
+        double sin_phi = sin(phi);
+        return glm::mat3(glm::orientation(direction, glm::vec3(0., 1., 0.))) * glm::vec3(cos(theta) * sin_phi, cos(phi), sin(theta) * sin_phi);
     }
     
 };
